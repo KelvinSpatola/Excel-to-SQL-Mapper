258761278974319b663459f262c7b006e978ff6d
Kelvin Spatola <kelvin.clark@ua.pt>
2023-01-03 15:10:44 +0000

    Deleted classes not used for this client

--- src/main/java/eu/aird/gta/TransactionalAnalisys.java
+++ src/main/java/eu/aird/gta/TransactionalAnalisys.java
@@ -10,7 +10,6 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.Properties;
-import java.util.Scanner;
 
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -19,7 +18,7 @@ import org.apache.poi.util.IOUtils;
 import com.monitorjbl.xlsx.StreamingReader;
 
 import eu.aird.gta.model.Product;
-import eu.aird.gta.model.TransactionCard;
+import eu.aird.gta.model.Transaction;
 import eu.aird.gta.util.GTAProperties;
 
 public class TransactionalAnalisys {
@@ -32,118 +31,80 @@ public class TransactionalAnalisys {
 		connectionProps.put("password", "admin123");
 		// mysql database
 		Connection conn = DriverManager.getConnection(
-				"jdbc:mysql://localhost/at_oncost?useSSL=false&createDatabaseIfNotExist=true", connectionProps);
+				"jdbc:mysql://localhost/at_makani?useSSL=false&createDatabaseIfNotExist=true", connectionProps);
 		return (conn);
 	}
 
 	public static void main(String[] args) {
 		IOUtils.setByteArrayMaxOverride(Integer.MAX_VALUE - 8);
 
-		boolean quit = false;
+//		loadProductStructureFile();
+		loadTransactionalFiles();
 
-		while (!quit) {
-			System.out.println(" ******************* GALP TRANSACTIONAL ANALYSIS ******************* ");
-			System.out.println(" --- MENU --- ");
-			System.out.println("1. Load transactional files");
-			System.out.println("2. Load product structure file");
-			System.out.println("0. Exit");
-			System.out.print("\nChoose option: ");
-			Scanner sc = new Scanner(System.in);
-			int menuOption = sc.nextInt();
-
-			switch (menuOption) {
-			case 0:
-				quit = true;
-				sc.close();
-				break;
-			case 1:
-				loadTransactionDetailsFile();
-				break;
-			case 2:
-//				loadProductStructureFile();
-				break;
-			}
-		}
 		System.out.println("Quiting...");
 		System.exit(0);
 	}
 
-	private static void loadTransactionDetailsFile() {
+	public static void loadTransactionalFiles() {
 		long start = System.currentTimeMillis();
-		
-		final int TOTAL_SHEETS = 1;
-		int workbookSheet = 0;
-		
-		String storesPath = props.get("data.stores");
-//		String storesPath = props.get("data.ways-of-payment");
-		File[] storeFiles = new File(storesPath).listFiles();
-		
+
+//		File[] storeFiles = new File(props.get("data.ways-of-payment")).listFiles();
+		File[] transactionalFiles = new File(props.get("data.stores")).listFiles();
+
 		try (Connection conn = getConnection()) {
 			conn.setAutoCommit(false);
 
-//			final String sql = "INSERT INTO payment_details"
-//					+ " (date, amount, card_type, card_number)"
-//					+ " VALUES (?, ?, ?, ?)";
-//			final String sql = "INSERT INTO transaction_details"
-//					+ " (ticket, date, amount, card_type)"
-//					+ " VALUES (?, ?, ?, ?)";
-//			final String sql = "INSERT INTO transactions"
-//					+ " (ticket, store, date, sku, value, quantity, unit, payment_type)"
-//					+ " VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-			final String sql = "INSERT INTO transaction_card"
-					+ " (transaction_ticket, card_number)"
-					+ " VALUES (?, ?)";
+			var sql = "INSERT INTO transactions"
+					+ " (ticket, store, store_desc, date, time, sku, sku_desc, value, quantity, total_value)"
+					+ " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 			PreparedStatement statement = conn.prepareStatement(sql);
 			statement.setFetchSize(Integer.MIN_VALUE);
-			InputStream inputStream;
 
-			while (workbookSheet < TOTAL_SHEETS) {
-				for (File storeFile : storeFiles) {
-					System.out.println("**********************************************************");
-					System.out.println("Reading file: " + storeFile.getName());
-					System.out.println("**********************************************************");
+			for (var file : transactionalFiles) {
+				System.out.println("**********************************************************");
+				System.out.println("Reading file: " + file.getName());
+				System.out.println("**********************************************************");
+
+				var inputStream = new FileInputStream(file);
+				var workbook = StreamingReader.builder().rowCacheSize(500).bufferSize(16384).open(inputStream);
+				var sheetItr = workbook.sheetIterator();
+				var sheetCount = workbook.getNumberOfSheets();
+				var sheetIndex = 1;
+				
+				while (sheetItr.hasNext()) {
+					var sheet = sheetItr.next();
+					var rowItr = sheet.iterator();
+					var lastRow = sheet.getLastRowNum();
+					var rowIndex = 1; // 1 - header
+					rowItr.next(); // skip the header row
 					
-					inputStream = new FileInputStream(storeFile);
-					Workbook workbook = StreamingReader.builder().rowCacheSize(500).bufferSize(16384).open(inputStream);
-					
-					Iterator<Row> rowIterator = workbook.getSheetAt(workbookSheet).iterator();
-					rowIterator.next(); // skip the header row
-					
-					int rowsCount = 1; // 1 - header
-					int totalRows = workbook.getSheetAt(workbookSheet).getLastRowNum();
-					TransactionCard transaction;
-					
-//					if (storeFile.getName().contains("Sulebhikhat payment details anonino - mod")) {
-//					if (storeFile.getName().contains("Sulebhikhat-2151 Transaction Details - mod")) {
-//					if (storeFile.getName().contains("Sulebhikhat-2151 Transaction Details Article wise - mod")) {
-					if (storeFile.getName().contains("Sulebhikhat  - etapa_1 - mod")) {
-						while (rowIterator.hasNext()) {
-							Row nextRow = rowIterator.next();
-							if (nextRow.getCell(0) == null)
-								break;
-							
-							transaction = new TransactionCard(nextRow);
-							transaction.setStatements(statement);
-							
-							statement.addBatch();
-							rowsCount++;
-							if (rowsCount % BATCH_SIZE == 0) {
-								statement.executeBatch();
-							}
-							System.out.println((rowsCount * 100) / totalRows + "% - row: " + rowsCount);
+					Transaction transaction;
+					while (rowItr.hasNext()) {
+						var row = rowItr.next();
+						if (row.getCell(0) == null) {
+							System.err.println("ERROR IN ROW " + rowIndex + ", cell 0!");
+							break;
 						}
+
+						transaction = new Transaction(row);
+						transaction.setStatements(statement);
+
+						statement.addBatch();
+						rowIndex++;
+						if (rowIndex % BATCH_SIZE == 0) {
+							statement.executeBatch();
+						}
+						System.out.println("Sheet: " + sheetIndex + "/" + sheetCount + " | row: " + rowIndex + " - " + (rowIndex * 100) / lastRow + "%");
 					}
 					inputStream.close();
 					statement.executeBatch();
 					conn.commit();
+					sheetIndex++;
 				}
-				
+
 				// execute the remaining queries
 				statement.executeBatch();
 				conn.commit();
-				
-				// let's run the next sheet
-				workbookSheet++;
 			}
 
 			printExecutionTime(start);
@@ -152,10 +113,10 @@ public class TransactionalAnalisys {
 			e.printStackTrace();
 		}
 	}
-	
-	private static void loadProductStructureFile() {
+
+	public static void loadProductStructureFile() {
 		long start = System.currentTimeMillis();
-		
+
 		String productsPath = props.get("data.product-structure");
 		System.out.println("path: " + productsPath);
 		File[] productFiles = new File(productsPath).listFiles();
@@ -163,50 +124,50 @@ public class TransactionalAnalisys {
 
 		try (Connection conn = getConnection()) {
 			conn.setAutoCommit(false);
-			
+
 			String sql = "INSERT INTO product "
-					+ "(sku, sku_desc, sub_cat, cat, macro) "
-					+ "VALUES (?, ?, ?, ?, ?)";
+					+ "(sku, sku_desc, sub_cat, cat, macro, pack_size, activation_date, disactivation_date) "
+					+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 			PreparedStatement statement = conn.prepareStatement(sql);
 			statement.setFetchSize(Integer.MIN_VALUE);
 			InputStream inputStream;
 			int productFileCount = 0;
-			
+
 			for (File productFile : productFiles) {
 				System.out.println("**********************************************************");
 				System.out.println("Reading file: " + productFile.getName());
 				System.out.println("**********************************************************");
-				
+
 				inputStream = new FileInputStream(productFile);
 				Workbook workbook = StreamingReader.builder().rowCacheSize(100).bufferSize(4096).open(inputStream);
-				
+
 				Iterator<Row> rowIterator = workbook.getSheetAt(0).iterator();
 				rowIterator.next(); // skip the header row
-				
+
 				productFileCount++;
 				int rowsCount = 1;
 				int totalRows = workbook.getSheetAt(0).getLastRowNum();
 				Product product;
-				
+
 				while (rowIterator.hasNext()) {
 					Row nextRow = rowIterator.next();
 					if (nextRow.getCell(0) == null) {
-						System.out.println("ERROR IN ROW " + rowsCount + " cell 0!");
+						System.err.println("ERROR IN ROW " + rowsCount + " cell 0!");
 						break;
 					}
-					
+
 					product = new Product(nextRow);
 					product.setStatements(statement);
-					
+
 					statement.addBatch();
 					rowsCount++;
 					if (rowsCount % BATCH_SIZE == 0) {
 						statement.executeBatch();
 					}
-					System.out.println("File: " + productFileCount + "/" + totalproductFiles +  " - " + productFile.getName() + " - "
-							+ (rowsCount * 100) / totalRows + "% - row: " + rowsCount);
+					System.out.println("File: " + productFileCount + "/" + totalproductFiles + " - "
+							+ productFile.getName() + " - " + (rowsCount * 100) / totalRows + "% - row: " + rowsCount);
 				}
-				
+
 				// execute the remaining queries
 				statement.executeBatch();
 				conn.commit();
@@ -224,12 +185,12 @@ public class TransactionalAnalisys {
 	}
 
 	private static void printExecutionTime(long startTime) {
-	    long end = System.currentTimeMillis();
-	    long time = (end - startTime) / 1000;
-	    long ss = time % 60;
-	    long mm = (time / 60) % 60;
-	    long hh = time / 3600;
-	    System.out.printf("\nIMPORT DONE in %02d.%02d.%02d ms\n", hh, mm, ss);
+		long end = System.currentTimeMillis();
+		long time = (end - startTime) / 1000;
+		long ss = time % 60;
+		long mm = (time / 60) % 60;
+		long hh = time / 3600;
+		System.out.printf("\nIMPORT DONE in %02d.%02d.%02d ms\n", hh, mm, ss);
 	}
-	
+
 }
--- src/main/java/eu/aird/gta/model/Product.java
+++ src/main/java/eu/aird/gta/model/Product.java
@@ -1,8 +1,9 @@
 package eu.aird.gta.model;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 
 import org.apache.poi.ss.usermodel.Row;
 
@@ -12,6 +13,9 @@ public class Product {
 	private String subCat;
 	private String cat;
 	private String macro;
+	private String packSize;
+	private Date activationDate;
+	private Date disactivationDate;
 	
 	public Product(Row row) {
 		this.sku = (int) row.getCell(0).getNumericCellValue();
@@ -19,22 +23,20 @@ public class Product {
 		this.subCat = row.getCell(2).getStringCellValue();
 		this.cat = row.getCell(3).getStringCellValue();
 		this.macro = row.getCell(4).getStringCellValue();
+		this.packSize = row.getCell(5).getStringCellValue();
+		this.activationDate = new Date(row.getCell(6).getDateCellValue().getTime());
+		Optional.ofNullable(row.getCell(7).getDateCellValue()).ifPresent(date -> disactivationDate = new Date(date.getTime()));
 	}
-	
-	public Product(ResultSet rs) throws SQLException {
-		this.sku = rs.getInt("sku");
-		this.skuDesc = rs.getString("sku_desc");
-		this.subCat = rs.getString("sub_cat");
-		this.cat = rs.getString("cat");
-		this.macro = rs.getString("macro");
-	}
-	
+
 	public void setStatements(PreparedStatement statement) throws SQLException {
 		statement.setInt(1, sku);
 		statement.setString(2, skuDesc);
 		statement.setString(3, subCat);
 		statement.setString(4, cat);
 		statement.setString(5, macro);
+		statement.setString(6, packSize);
+		statement.setDate(7, activationDate);
+		statement.setDate(8, disactivationDate);
 	}
 
 }
